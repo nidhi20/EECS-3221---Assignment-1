3bed43efeda89d15f3bf536461175ba0e7e00736
Nidhi Sarathmohan <nidz.sarath@gmail.com>
2016-03-20 21:59:12 -0400

    Update thread.c

--- thread.c
+++ thread.c
@@ -2,10 +2,8 @@
  #Family Name: Sarathmohan
  #Given Name: Nidhi
  #Section: Z
- #Student number: 211934619
- #cse login: cse22011
- 
- */
+*/
+
 #include <stdio.h>
 #include <ctype.h>
 #include <string.h>
